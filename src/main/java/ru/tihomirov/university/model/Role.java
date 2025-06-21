52a2f5d5131e5edd098b539f3fe4992578e4f419
Daniil Tihomirov <daniiltihomirov003@gmail.com>
2025-06-21 18:49:40 +0300

    Создал проект, подключил необходимые зависимости, создал модели.

---
+++ src/main/java/ru/tihomirov/university/model/Role.java
@@ -0,0 +1,19 @@
+package ru.tihomirov.university.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Entity
+@Table(name = "roles")
+@Data
+@Accessors(chain = true)
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "role_id_seq")
+    @SequenceGenerator(name = "role_id_seq", sequenceName = "role_id_seq", allocationSize = 1)
+    private Long id;
+
+    private String name;
+}
